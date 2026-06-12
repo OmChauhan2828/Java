29cb260e3e5004f46aad091d224f28a023e4f874
om chauhan <omchauhan5020@gmail.com>
2026-06-12 21:19:33 +0530

    variables in the java

--- JavaBasics.java
+++ JavaBasics.java
@@ -1,6 +1,11 @@
 public class JavaBasics {
     public static void main(String args[]) {
-        System.out.println("Hello, World!");
+        int a = 10;
+        int b = 20;
+        System.out.println(a);
+        System.out.println(b);
+        String name = "Virat";
+        System.out.println(name);
 
     }
 }
