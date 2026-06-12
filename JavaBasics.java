0874138cdc676ab93cbc70f4bd60712ba53c40c7
om chauhan <omchauhan5020@gmail.com>
2026-06-12 21:38:56 +0530

    data types in java

--- JavaBasics.java
+++ JavaBasics.java
@@ -1,11 +1,26 @@
 public class JavaBasics {
     public static void main(String args[]) {
-        int a = 10;
-        int b = 20;
-        System.out.println(a);
-        System.out.println(b);
-        String name = "Virat";
-        System.out.println(name);
+        // variables in java
+        // int a = 10;
+        // int b = 20;
+        // System.out.println(a);
+        // System.out.println(b);
+        // String name = "Virat";
+        // System.out.println(name);
 
+        // data types in java
+        
+        byte a = 10;
+        System.out.println(a);
+        char ch ='A';
+        System.out.println(ch);
+        boolean var = true;
+        System.out.println(var);
+        int num = 100;
+        System.out.println(num);
+        float decimal =8.9;
+        short s = 1000;
+        System.out.println(decimal);
+        System.out.println(s);
     }
 }
