306bd995eb7b642d38247d13d6a7007487c7bff2
om chauhan <omchauhan5020@gmail.com>
2026-06-14 21:59:05 +0530

    sum of a & b by taking input from user

--- JavaBasics.java
+++ JavaBasics.java
@@ -1,32 +1,52 @@
-public class JavaBasics {
-    public static void main(String args[]) {
-        // variables in java
-        // int a = 10;
-        // int b = 20;
-        // System.out.println(a);
-        // System.out.println(b);
-        // String name = "Virat";
-        // System.out.println(name);
+// import java.util.Scanner;
+
+// public class JavaBasics {
+//     public static void main(String args[]) {
+//         try (Scanner scanner = new Scanner(System.in)) {
+//             String input = scanner.next();
+//             System.out.println(input);
+
+//             // variables in java
+//         // int a = 10;
+//         // int b = 20;
+//         // System.out.println(a);
+//         // System.out.println(b);
+//         // String name = "Virat";
+//         // System.out.println(name);
 
 
-        // data types in java
-        // byte a = 10;
-        // System.out.println(a);
-        // char ch ='A';
-        // System.out.println(ch);
-        // boolean var = true;
-        // System.out.println(var);
-        // int num = 100;
-        // System.out.println(num);
-        // float decimal =8.9;
-        // short s = 1000;
-        // System.out.println(decimal);
-        // System.out.println(s);
+//         // data types in java
+//         // byte a = 10;
+//         // System.out.println(a);
+//         // char ch ='A';
+//         // System.out.println(ch);
+//         // boolean var = true;
+//         // System.out.println(var);
+//         // int num = 100;
+//         // System.out.println(num);
+//         // float decimal =8.9;
+//         // short s = 1000;
+//         // System.out.println(decimal);
+//         // System.out.println(s);
 
-        // Sum of two numbers
-        int a=10;
-        int b=20;
-        int sum =a+b;
-        System.out.println("The sum of a and b is: " + sum);
-    }
-}
+//         // Sum of two numbers
+//             // int a=10;
+//             // int b=20;
+//             // int sum =a+b;
+//             // System.out.println("The sum of a and b is: " + sum);
+//         }
+//     }
+// }
+
+// Sum of a & b by using user input
+// import java.util.*;
+// public class JavaBasics {
+//     public static void main(String args[]) {
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+//         int b = sc.nextInt();
+//         int sum = a + b;
+//         System.out.println("The sum of a and b is: " + sum);
+
+//     }
+// }
