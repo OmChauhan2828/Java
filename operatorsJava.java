4ebcf26ba948f3ee3671b3df9d271faf3bcb7df2
om chauhan <omchauhan5020@gmail.com>
2026-06-16 22:20:45 +0530

    Arithematic operators in Java are used to perform basic mathematical operations such as addition, subtraction, multiplication, division, and modulus. Here is an example of how to use arithmetic operators in Java:

--- operatorsJava.java
+++ operatorsJava.java
@@ -1,3 +1,10 @@
+import java.util.*;
 public class operatorsJava {
-    
+    public static void main(String args[]) {
+        int A= 10;
+        int B=20;
+        // Arithmetic operators
+         System.out.println("The sum of A and B is: " + (A+B));       
+       
+    }
 }
