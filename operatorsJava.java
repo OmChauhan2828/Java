a6e1544e542e1a8b08b003f7b3da76d7a7fe8f32
om chauhan <omchauhan5020@gmail.com>
2026-06-16 22:32:33 +0530

    relational Operators in Java

--- operatorsJava.java
+++ operatorsJava.java
@@ -4,7 +4,9 @@ public class operatorsJava {
         int A= 10;
         int B=20;
         // Arithmetic operators
-         System.out.println("The sum of A and B is: " + (A+B));       
+         System.out.println("The sum of A and B is: " + (A+B));   
+        // Relational operators
+        System.out.println("A and B are equal:" + (A==B));
        
     }
 }
