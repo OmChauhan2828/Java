e2816d27b29b887eb49100f60d1a0b476114ce6d
om chauhan <omchauhan5020@gmail.com>
2026-06-17 22:40:38 +0530

    print the larger of  number

--- Java2.java
+++ Java2.java
@@ -1,16 +1,28 @@
 import java.util.*;
 public class Java2 {
     public static void main(String args[]) {
-    int age=16;
-    if(age>=18){
-        System.out.println("You are eligible to vote");
-    }
-    if(age>13 && age<18){
-     System.out.println("You are a teenager");
-    }
-    else{
-        System.out.println("not adult");   
-    } 
-    }
+    // int age=16;
+    // if(age>=18){
+    //     System.out.println("You are eligible to vote");
+    // }
+    // if(age>13 && age<18){
+    //  System.out.println("You are a teenager");
+    // }
+    // else{
+    //     System.out.println("not adult");   
+    // } 
+    // }
     
+    // print the largest of two numbers
+
+    int A=10;
+    int B=20;
+
+    if(A>B) {
+        System.out.println("A is larger");
+    }
+    else {
+        System.out.println("B is larger");
+    }
+}
 }
