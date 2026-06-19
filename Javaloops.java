56f315fbf601b15961bd9f42772ee1890674263a
om chauhan <omchauhan5020@gmail.com>
2026-06-19 19:02:07 +0530

    while loop in java

--- Javaloops.java
+++ Javaloops.java
@@ -1,4 +1,11 @@
 import java.util.*;
 public class Javaloops {
-    
-}
+    public static void main(String args[]){
+        int count = 0;
+        while(count <=50){
+            System.out.println("Hello World");
+            count++;
+        }
+        }
+    }
+
