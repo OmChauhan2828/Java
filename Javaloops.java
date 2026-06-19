e914f6a395507f8f93a5b1d6880df4bb923aab6d
om chauhan <omchauhan5020@gmail.com>
2026-06-19 22:29:20 +0530

    do while loop in the Java

--- Javaloops.java
+++ Javaloops.java
@@ -18,14 +18,41 @@ import java.util.*;
 
 
 // Print number from 1 to n using while loop by taking input from the user;
+// public class Javaloops {
+//     public static void main(String args[]){
+//         Scanner sc = new Scanner(System.in);
+//         int range = sc.nextInt();
+//         int count=1;
+//         while(count<=range){
+//             System.out.println(count);
+//             count++;
+//         }
+//     }
+
+
+// For loop
+
+// public class Javaloops {
+//     public static void main(String args[]){
+//         // for(int i=1;i<=10;i++){
+//         //     System.out.println(i);
+//         // }
+
+//         // Print Square pattern
+//         // for(int line=1;line<=4;line++){
+//         //     System.out.println("****");
+//         // }
+//     }
+// }
+
+
+// do while loop
 public class Javaloops {
     public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        int range = sc.nextInt();
         int count=1;
-        while(count<=range){
+        do{
             System.out.println(count);
             count++;
-        }
+        }while(count<=10);
     }
-} 
+}
