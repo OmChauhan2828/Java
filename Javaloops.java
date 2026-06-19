c1c62a61742f7a54a54d74fd01946c5dc3eec736
om chauhan <omchauhan5020@gmail.com>
2026-06-19 19:16:57 +0530

    print number from 1 to n using while loop and taking the input from the user

--- Javaloops.java
+++ Javaloops.java
@@ -1,18 +1,31 @@
 import java.util.*;
+// public class Javaloops {
+//     public static void main(String args[]){
+//         // int count = 0;
+//         // while(count <=50){
+//         //     System.out.println("Hello World");
+//         //     count++;
+//         // }
+
+//         // print number from 1 to 10 using while loop
+//         int count=1;
+//         while(count<=10){
+//             System.out.println(count);
+//             count++;
+//         }
+//         }
+//     }
+
+
+// Print number from 1 to n using while loop by taking input from the user;
 public class Javaloops {
     public static void main(String args[]){
-        // int count = 0;
-        // while(count <=50){
-        //     System.out.println("Hello World");
-        //     count++;
-        // }
-
-        // print number from 1 to 10 using while loop
+        Scanner sc = new Scanner(System.in);
+        int range = sc.nextInt();
         int count=1;
-        while(count<=10){
+        while(count<=range){
             System.out.println(count);
             count++;
         }
-        }
     }
-
+} 
