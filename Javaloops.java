3934410b699be938d4bf979d566299f5eb5e4ae6
om chauhan <omchauhan5020@gmail.com>
2026-06-19 19:09:55 +0530

    print number from 1 to 10 using while loop

--- Javaloops.java
+++ Javaloops.java
@@ -1,9 +1,16 @@
 import java.util.*;
 public class Javaloops {
     public static void main(String args[]){
-        int count = 0;
-        while(count <=50){
-            System.out.println("Hello World");
+        // int count = 0;
+        // while(count <=50){
+        //     System.out.println("Hello World");
+        //     count++;
+        // }
+
+        // print number from 1 to 10 using while loop
+        int count=1;
+        while(count<=10){
+            System.out.println(count);
             count++;
         }
         }
