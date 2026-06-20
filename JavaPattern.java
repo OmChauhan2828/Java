439434d5a3eb0501336591b76e7bb4c734b84754
om chauhan <omchauhan5020@gmail.com>
2026-06-20 21:01:28 +0530

    Pattern in Java by using the nested loops

---
+++ JavaPattern.java
@@ -0,0 +1,22 @@
+public  class JavaPattern {
+   public static void main(String args[]){
+
+    for(int line=1;line<=4;line++){
+        for(int star=1;star<=line;star++){
+            System.out.print("*");
+        }
+        System.out.println();
+
+    }
+
+
+//     for(int line=1;line<=4;line++){
+//         for(int star=1;star<=5-line;star++){
+//             System.out.print("*");
+//         }
+//         System.out.println();
+//     }
+//    }
+    
+}
+}
