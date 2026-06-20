22e219d62d04158cd9dcd1610e641707403c3c0e
om chauhan <omchauhan5020@gmail.com>
2026-06-20 21:18:25 +0530

    Character Pattern in Java

--- JavaPattern.java
+++ JavaPattern.java
@@ -1,22 +1,47 @@
 public  class JavaPattern {
    public static void main(String args[]){
 
-    for(int line=1;line<=4;line++){
-        for(int star=1;star<=line;star++){
-            System.out.print("*");
-        }
-        System.out.println();
+    // for(int line=1;line<=4;line++){
+    //     for(int star=1;star<=line;star++){
+    //         System.out.print("*");
+    //     }
+    //     System.out.println();
 
-    }
+    // }
 
 // Inverted Star Pattern
-    for(int line=1;line<=4;line++){
-        for(int star=1;star<=4-line+1;star++){
-            System.out.print("*");
-        }
-        System.out.println();
-    }
-   }
+//     for(int line=1;line<=4;line++){
+//         for(int star=1;star<=4-line+1;star++){
+//             System.out.print("*");
+//         }
+//         System.out.println();
+//     }
+//    }
     
+// Half Pyramid Pattern
+// int n = 5;
+// for(int line=1;line<=n;line++){
+//     for(int star=1;star<=line;star++){
+//         System.out.print(star);
+//     }
+//     System.out.println();
+// }
+// }
+
+
+// character Pattern
+
+int n=5;
+char ch='A';
+for(int line=1;line<=n;line++){
+    for(int j=1;j<=line;j++){
+        System.out.print(ch);
+        ch++;
+    }
+    System.out.println();
 }
+}
+}
+
+
 
