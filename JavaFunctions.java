2a071538c6a5dd5aa76b891a97ac5c500b9e1eb6
om chauhan <omchauhan5020@gmail.com>
2026-06-21 22:46:34 +0530

    changes to be committed:

--- JavaFunctions.java
+++ JavaFunctions.java
@@ -2,7 +2,7 @@ public class JavaFunctions {
     public static void printhello(){
         System.out.println("Hello, World!");
         System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
+        System.out.println("Om Chauhan this side!");
 
     }
     public static void main(String[] args) {
