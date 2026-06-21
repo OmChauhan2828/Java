4d91dd11c274651423b7ea1904e7e5d69f2b9ed7
om chauhan <omchauhan5020@gmail.com>
2026-06-21 13:00:37 +0530

    Functions and methods in Java

---
+++ JavaFunctions.java
@@ -0,0 +1,11 @@
+public class JavaFunctions {
+    public static void printhello(){
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+
+    }
+    public static void main(String[] args) {
+        printhello();
+    }
+}
