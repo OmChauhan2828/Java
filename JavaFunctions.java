f31f4eccfda8208b38b3194fb4ec44f7fef236da
om chauhan <omchauhan5020@gmail.com>
2026-06-22 23:10:37 +0530

    product of two numbers using function

--- JavaFunctions.java
+++ JavaFunctions.java
@@ -1,11 +1,26 @@
-public class JavaFunctions {
-    public static void printhello(){
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World");
-        System.out.println("Om Chauhan this side!");
+// public class JavaFunctions {
+//     public static void product(){
+//         System.out.println("Hello, World!");
+//         System.out.println("Hello, World");
+//         System.out.println("Om Chauhan this side!");
 
-    }
-    public static void main(String[] args) {
-        printhello();
-    }
-}
+//     }
+//     public static void main(String[] args) {
+//         printhello();
+//     }
+
+// product of two numbers using functions
+//  public class JavaFunctions {
+//     public static void multiply(int a, int b){
+//         int product = a*b;
+//         return;
+//     }
+
+//     public static void main(String[]args){
+//         int a = 10;
+//         int b=20;
+//         int prod = multiply (a,b);
+//         System.out.println("The product of a and b is: " + prod);
+
+//     }
+//  }
---
+++ Javaunctions.java
@@ -0,0 +1,13 @@
+// public class JavaFunctions {
+//     public static void product(){
+//         System.out.println("Hello, World!");
+//         System.out.println("Hello, World");
+//         System.out.println("Om Chauhan this side!");
+
+//     }
+//     public static void main(String[] args) {
+//         printhello();
+//     }
+ public class JavaFunctions {
+    
+ }
