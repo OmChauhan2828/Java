51fef6bdc9166fdb809a045d1795e49100c993c3
om chauhan <omchauhan5020@gmail.com>
2026-06-25 23:58:33 +0530

    largest in the array

--- JavaArray.java
+++ JavaArray.java
@@ -20,27 +20,46 @@ import java.util.*;
 
 
 // linear Search
+// public class JavaArray {
+//     public static int linearsearch(int number[], int key) {
+//         for (int i = 0; i < number.length; i++) {
+//             if (number[i] == key) {
+//                 return i;
+//             }
+//         }
+//         return -1;
+//     }
+//     public static void main(String args[]) {
+//         int numbers[] = {2, 4, 6, 8, 10, 12, 14};
+//         int key = 14;
+
+//         int index = linearsearch(numbers, key);
+//         if (index == -1) {
+//             System.out.println("Not found");
+//         } else {
+//             System.out.println("element at index " + index);
+//         }
+
+//         System.out.println();
+//     }
+
+// Largest Number in the array
 public class JavaArray {
-    public static int linearsearch(int number[], int key) {
-        for (int i = 0; i < number.length; i++) {
-            if (number[i] == key) {
-                return i;
+
+    public static int getlargest(int numbers[]){
+        int largest = Integer.MIN_VALUE;
+
+        for(int i=0;i<numbers.length;i++){
+            if(largest<numbers[i]){
+                largest = numbers[i];
             }
         }
-        return -1;
+        return largest;
     }
-
     public static void main(String args[]) {
-        int numbers[] = {2, 4, 6, 8, 10, 12, 14};
-        int key = 14;
+        int numbers[]={1,2,3,6,7};
+        System.out.println("the largest number in the array is"+ getlargest(numbers));
 
-        int index = linearsearch(numbers, key);
-        if (index == -1) {
-            System.out.println("Not found");
-        } else {
-            System.out.println("element at index " + index);
-        }
 
-        System.out.println();
     }
 }
