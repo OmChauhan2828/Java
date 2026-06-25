f16b68f5025708d5d2f86651a42340310b81fddf
om chauhan <omchauhan5020@gmail.com>
2026-06-25 22:56:42 +0530

    insert data into an array

--- JavaArray.java
+++ JavaArray.java
@@ -1,5 +1,19 @@
+import java.util.*;
+
 public class JavaArray {
     public static void main(String[] args){
         int[] marks = new int[5];
+
+        Scanner sc = new Scanner(System.in);
+
+        // int phy =sc.nextInt();
+        marks[0]=sc.nextInt();
+        marks[1]=sc.nextInt();
+        marks[2]=sc.nextInt();
+
+        System.out.println("phy :" + marks[0]);
+        System.out.println("phy :" + marks[0]);
+        System.out.println("phy :" + marks[0]);
+
     }
 }
